af76c10de9991d8dc0739bc63d97e11efd361124
pastordesoles <xavi.sans.b@gmail.com>
2022-08-11 15:09:45 +0200

    fix continuous operations

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const clearScreen = () => {
   document.getElementById("result").value = "";
   num1 = undefined;
   num2 = undefined;
-  total = undefined
-  operand = undefined
+  total = undefined;
+  operand = undefined;
 };
 
 const display = (value) => {
@@ -17,24 +17,27 @@ const display = (value) => {
 };
 
 const addPoint = () => {
-    let hasPoint = document.getElementById("result").value
-    if (!hasPoint.includes('.')){
-        display(".");
-    }
+  let hasPoint = document.getElementById("result").value;
+  if (!hasPoint.includes(".")) {
+    display(".");
+  }
 };
 
 const getNumberTwo = () => {
-    num2 = Number(document.getElementById("result").value);
-    operations()
-}
+  num2 = Number(document.getElementById("result").value);
+  operations();
+};
 
 const minus = () => {
-    if (total !== undefined) {
-        num1 = total
-        operand = "-"
-        document.getElementById("result").value = ""
-        getNumberTwo()
-    }
+  if (total !== undefined && operand === "=") {
+    num1 = total;
+    operand = "-";
+    document.getElementById("result").value = "";
+    getNumberTwo();
+  } else if (num1 !== undefined && operand !== "=") {
+    num2 = Number(document.getElementById("result").value);
+    operations();
+  }
   num1 = Number(document.getElementById("result").value);
   operand = "-";
   document.getElementById("result").value = "";
@@ -42,10 +45,13 @@ const minus = () => {
 
 const plus = () => {
   if (total !== undefined && operand === "=") {
-    num1 = total
-    operand = "+"
-    document.getElementById("result").value = ""
+    num1 = total;
+    operand = "+";
+    document.getElementById("result").value = "";
     getNumberTwo();
+  } else if (num1 !== undefined && operand !== "=") {
+    num2 = Number(document.getElementById("result").value);
+    operations();
   }
   num1 = Number(document.getElementById("result").value);
   operand = "+";
@@ -53,24 +59,30 @@ const plus = () => {
 };
 
 const mult = () => {
-    if (total !== undefined) {
-        num1 = total
-        operand = "*"
-        document.getElementById("result").value = ""
-        getNumberTwo()
-    }
+  if (total !== undefined && operand === "=") {
+    num1 = total;
+    operand = "*";
+    document.getElementById("result").value = "";
+    getNumberTwo();
+  } else if (num1 !== undefined && operand !== "=") {
+    num2 = Number(document.getElementById("result").value);
+    operations();
+  }
   num1 = Number(document.getElementById("result").value);
   operand = "*";
   document.getElementById("result").value = "";
 };
 
 const div = () => {
-    if (total !== undefined) {
-        num1 = total
-        operand = "/"
-        document.getElementById("result").value = ""
-        getNumberTwo()
-    }
+  if (total !== undefined && operand === "=") {
+    num1 = total;
+    operand = "/";
+    document.getElementById("result").value = "";
+    getNumberTwo();
+  } else if (num1 !== undefined && operand !== "=") {
+    num2 = Number(document.getElementById("result").value);
+    operations();
+  }
   num1 = Number(document.getElementById("result").value);
   operand = "/";
   document.getElementById("result").value = "";
@@ -92,89 +104,89 @@ const operations = () => {
       document.getElementById("result").value = "";
       total = num1 + num2;
       display(total);
-      operand = "="
+      operand = "=";
       break;
     case "-":
       document.getElementById("result").value = "";
       total = num1 - num2;
       display(total);
-      operand = "="
+      operand = "=";
       num1 = total;
       break;
     case "*":
       document.getElementById("result").value = "";
       total = num1 * num2;
       display(total);
-      operand = "="
+      operand = "=";
       num1 = total;
       break;
     case "/":
       document.getElementById("result").value = "";
       total = num1 / num2;
       display(total);
-      operand = "="
+      operand = "=";
       num1 = total;
       break;
   }
 };
 
 const numberClick = (number) => {
-    if (operand === "=") {
-        clearScreen()
-    }
-    display(number);
-}
+  if (operand === "=") {
+    clearScreen();
+  }
+  display(number);
+};
 
 document.addEventListener("keydown", (event) => {
-    if (event.key === "1") {
-        numberClick('1');
-    }
-    if (event.key === "2") {
-        numberClick('2');
-    }
-    if (event.key === "3") {
-        numberClick('3');
-    }
-    if (event.key === "4") {
-        numberClick('4');
-    }
-    if (event.key === "5") {
-        numberClick('5');
-    }
-    if (event.key === "6") {
-        numberClick('6');
-    }
-    if (event.key === "7") {
-        numberClick('7');
-    }
-    if (event.key === "8") {
-        numberClick('8');
-    }
-    if (event.key === "9") {
-        numberClick('9');
-    }
-    if (event.key === "0") {
-        numberClick('0');
-    }
-    if (event.key === ".") {
-      addPoint();
-    }
-    if (event.key === "+") {
-      plus();
-    }
-    if (event.key === "-") {
-      minus();
-    }
-    if (event.key === "*") {
-      mult();
-    }
-    if (event.key === "/") {
-      div();
-    }
-    if (event.key === "Enter") {
-      calculate();
-    }
-    if (event.key === "c") {
-      clearScreen();
-    }
-  });
+  if (event.key === "1") {
+    numberClick("1");
+  }
+  if (event.key === "2") {
+    numberClick("2");
+  }
+  if (event.key === "3") {
+    numberClick("3");
+  }
+  if (event.key === "4") {
+    numberClick("4");
+  }
+  if (event.key === "5") {
+    numberClick("5");
+  }
+  if (event.key === "6") {
+    numberClick("6");
+  }
+  if (event.key === "7") {
+    numberClick("7");
+  }
+  if (event.key === "8") {
+    numberClick("8");
+  }
+  if (event.key === "9") {
+    numberClick("9");
+  }
+  if (event.key === "0") {
+    numberClick("0");
+  }
+  if (event.key === ".") {
+    addPoint();
+  }
+  if (event.key === "+") {
+    plus();
+  }
+  if (event.key === "-") {
+    minus();
+  }
+  if (event.key === "*") {
+    mult();
+  }
+  if (event.key === "/") {
+    div();
+  }
+  if (event.key === "=") {
+    calculate();
+  }
+  if (event.key === "c") {
+    clearScreen();
+  }
+});
